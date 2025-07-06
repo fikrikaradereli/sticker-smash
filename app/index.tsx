f22241d689dce20bcb4176890c12dfd6b399ae50
Fikri Karadereli <fikrikaradereli@hotmail.com>
2025-07-06 14:44:21 +0300

    Navigasyon eklendi.

---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Selamunaleyküm</Text>
+      <Link href={"/about"} style={styles.button}>
+        Go to About Screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white"
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "white",
+  }
+});
